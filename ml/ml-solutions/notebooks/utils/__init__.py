a55531b6abd55d1fd491fa5aa2eeeacd75a5177a
us241098 <us241077@gmail.com>
2021-12-29 14:13:04 +0530

    add: duckling_entity test

--- ml/ml-solutions/notebooks/utils/__init__.py
+++ ml/ml-solutions/notebooks/utils/__init__.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.metrics import classification_report
 import json
 from sklearn.metrics import f1_score
-
+import json
 
 def preprocess(df):
     texts = []
@@ -41,3 +41,39 @@ def test_slu_model(test_csv, service_url):
         print("Pass")
     else:
         print(":/ Lets Try Again")
+
+
+def test_duckling_entities(test_json, service_url):
+    predicted_entities = []
+    true_entities = []
+    f = open(test_json)
+    tests = json.load(f)
+    for i in tests:
+        req_obj = {}
+        req_obj["alternatives"] = i["alternatives"]
+        req_obj["context"] = {}
+        req_obj["context"]["current_state"] = "test_state"
+        API_ENDPOINT = service_url
+        data = json.loads(json.dumps(req_obj))
+        headers = {"Content-type": "application/json", "Accept": "text/plain"}
+        r = requests.post(url=API_ENDPOINT, json=data, headers=headers)
+        response = r.json()
+        true_entities.append(i["entity_type"])
+        try:
+            predicted_entities.append(response["response"]["entities"][0]["entity_type"])
+        except:
+            predicted_entities.append("none")
+    
+    pass_flag = True 
+    for i in range(len(true_entities)):
+        if true_entities[i] != predicted_entities[i]:
+            pass_flag = False
+            print("failed test case:")
+            print("true entity_type:", true_entities[i])
+            print("predicted entity_type:", predicted_entities[i])
+            print("description: ", tests[i]["description"])
+        
+    if pass_flag:
+        print("All tests passed!")
+
+
